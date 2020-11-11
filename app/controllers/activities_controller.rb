d3f330adeaf65e59a5218e46ec5e23360ac21c15
Michael-Lee-1994 <michael.lee.1994.flatiron@gmail.com>
2020-11-11 13:33:43 -0800

    added activity functionality

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -9,22 +9,65 @@ class ActivitiesController < ApplicationController
     end
 
     def new
+        byebug
         @activity = Activity.new
     end
 
     def create
-        @activity = Activity.new(activity_params)
-        if @activity.save
-            session[:activity_id] = @activity.id
-            redirect_to user_path(current_user)
+        if params[:food]
+            @activity = current_choice.choice_activities.create(activity_id: get_random("food").id)
+            if @activity.save
+                redirect_to activities_path
+            else
+                render :new
+            end
+        elsif params[:movie]
+            @activity = current_choice.choice_activities.create(activity_id: get_random("movie").id)
+            if @activity.save
+                redirect_to activities_path
+            else
+                render :new
+            end
         else
-            render :new
+            @activity = current_choice.activities.create(activity_params)
+            if @activity.save
+                # session[:activity_id] = @activity.id
+                redirect_to user_path(current_user)
+            else
+                render :new
+            end
+
         end
     end
+
+    # def food
+    #     @activity = current_choice.choice_activities.create(activity_id: get_random("food").id)
+    #     if @choice.save
+    #         redirect_to activities_path
+    #     else
+    #         render :new
+    #     end
+    # end
 
     private
     def activity_params
         params.require(:activity).permit(:name, :category, :genre_cuisine)
     end
+
+    def get_random(type)
+        activity = nil
+        if type == "food"
+            activity = Activity.where("category LIKE ?", "food").sample
+        elsif type == "movie"
+            activity = Activity.where("type LIKE ?", "movie").sample
+        else
+            activity = Activity.find(Random.new(100))
+        end 
+        p activity
+    end
+
+    # def random_params
+    #     params.require(:choice_activites).permit(:choice_id, :activity_id)
+    # end
 
 end
--- app/controllers/choice_activities_controller.rb
+++ app/controllers/choice_activities_controller.rb
@@ -13,8 +13,30 @@ class ChoiceActivitiesController < ApplicationController
         end
     end
 
+    # def random_food
+    #     @activity = current_user.activities.create(get_random("food"))
+    #     if @choice.save
+    #         redirect_to activities_path
+    #     else
+    #         render :new
+    #     end
+    # end
+    
+
     private
     def ca_params
         params.require(:choice_activity).permit(:activity_id, :choice_id)
     end
+
+    # def get_random(type)
+    #     activity = nil
+    #     if type == "food"
+    #         activity = Activity.where("type LIKE ?", "food").sample
+    #     elsif type == "movie"
+    #         activity = Activity.where("type LIKE ?", "movie").sample
+    #     else
+    #         activity = Activity.find(Random.new(100))
+    #     end 
+    #     activity
+    # end
 end
