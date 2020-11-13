74a0aa161b7f885a08550854d8ba350cc302c929
Kim Nguyen <nnhk23@gmail.com>
2020-11-12 16:24:15 -0800

    add destroy choice, add activity button

--- app/models/choice.rb
+++ app/models/choice.rb
@@ -2,7 +2,7 @@ class Choice < ApplicationRecord
     has_many :user_choices, dependent: :destroy
     has_many :users, through: :user_choices
 
-    has_many :choice_activities
+    has_many :choice_activities, dependent: :destroy
     has_many :activities, through: :choice_activities
 
     validates :location_zip, length: {minimum: 5}
